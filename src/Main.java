1c4f1d16d571521a9f40bd49c7d3c517a1889c13
Osamiee <osamie.ogbewe@stu.ocsb.ca>
2024-09-19 14:39:33 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -27,10 +27,33 @@ public class Main {
 		int time = in.nextInt();
 		//flush
 		in.nextLine();
+		System.out.println("Give me a store name");
+		String store = in.nextLine();
+		//flush
+		in.nextLine();
 		System.out.println("Give me a decimal number");
 		double money = in.nextDouble();
-		System.out.println("An old man named "+name+" and aged "+age+" was walking across "+street+" at "+time+" o'clock. He bought something from a grocery store and his total was $"+money+". ");
-		
+		//flush 
+		in.nextLine();
+		System.out.println("Give me a name");
+		String name1 = in.nextLine();
+		//flush
+		in.nextLine();
+		System.out.println("Give me an age");
+		int age1 = in.nextInt();
+		//flush
+		in.nextLine();
+		System.out.println("Give me a company name");
+		String company = in.nextLine();
+		//flush
+		in.nextLine();
+		System.out.println("Give me a number");
+		int money1 = in.nextInt();
+		System.out.println("An old man named "+name+" and aged "+age+" was walking across "+street+" at "+time+" o'clock.)"
+				+ "( He bought something from " + store + " and his total was $"+money+".)"
+						+ "( He left the store and seen his old friend "+name1+".)"
+								+ "( His friend was now " +age1+ " and was the owner of "+company+".)"
+										+ "( "+ name1+" gave "+ name +" $"+ money1 +" as a gift.");
 
 	}
 
